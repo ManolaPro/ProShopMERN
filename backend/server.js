f37c90f25ebb878ab99181c8b12c2e72b6f0b86f
Alexey_Chernyshev <blacklex2012@gmail.com>
2021-01-25 14:21:48 +0300

    user auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './confg/db.js';
-import productRoutes from './routes/productRoutes.js'
-
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 dotenv.config()
 
 connectDB()
 
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
